60006fd40af1dc3511cb6ef185885e18f975878c
willieLjohnson <liwa.johnson@gmail.com>
2019-05-18 23:16:07 -0400

    Yea still trying to solve the same damn heroku bug

--- server.js
+++ server.js
@@ -8,17 +8,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 require('./routes/email')(app);
 
-if (process.env.ENV === 'production') {
-  //Express will serve up production assets
-  app.use(express.static('client/build'));
+//Express will serve up production assets
+app.use(express.static('client/build'));
 
-  //Express will serve up the index.html file
-  //if it doesn't recognize the route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//Express will serve up the index.html file
+//if it doesn't recognize the route
+const path = require('path');
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const port = envPort || 5000;
 
