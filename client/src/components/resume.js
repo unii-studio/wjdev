0f208fc61cc74598558edc9a52e49f2f39b3014e
willieLjohnson <liwa.johnson@gmail.com>
2019-05-20 13:11:37 -0400

    Finish resume.js refactor

--- client/src/components/resume.js
+++ client/src/components/resume.js
@@ -5,101 +5,99 @@ import Experience from './experience';
 import Skills from './skills';
 
 import Portrait from '../blocks/Portrait';
+import History from '../blocks/History';
+
 import P from '../elements/P';
+import H2 from '../elements/H2';
 import H5 from '../elements/H5';
 
 const Resume = () => (
-  <div>
-    <Grid>
-      <Cell col={4}>
-        <Portrait>
-          <Portrait.Image />
-          <Portrait.Name />
-          <Portrait.Title />
-          <Portrait.Split />
-          <P>
-            I'm a software engineer who specializes in iOS Development. With a
-            variety of projects under my belt I have experience using many
-            different technologies. I love to develop video games, productivity
-            apps, augmented reality apps, and content driven websites.
-          </P>
-          <Portrait.Split />
-          <H5>Address</H5>
-          <P>
-            <a
-              href="https://www.google.com/maps/place/MONTGOMRY+VLG,+MD+20879/@39.1708499,-77.1858614,14.74z/data=!4m5!3m4!1s0x89b62b579a467b6d:0xe2ea76d8df38b7a8!8m2!3d39.1733025!4d-77.1772737"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              Gaithersburg, MD 20879
-            </a>
-          </P>
-          <H5>Phone</H5>
-          <P>
-            <a
-              href="tel:240-550-5589"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              (240) 550-5589
-            </a>
-          </P>
-          <H5>Email</H5>
-          <P>
-            <a
-              href={"mailto:liwa.johnson@gmail.com?subject=Let's get in touch"}
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              liwa.johnson@gmail.com
-            </a>
-          </P>
-          <H5>Web</H5>
-          <P>
-            <a
-              href="http://unii.design"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              unii.design
-            </a>
-          </P>
-          <Portrait.Split />
-        </Portrait>
-      </Cell>
-      <Cell className="resume-right-col" col={8}>
-        {/* Skills */}
-        <h2 style={{ fontFamily: 'Raleway' }}>Skills</h2>
-        <Skills skill="iOS/Swift" progress={100} />
-        <Skills skill="Unity" progress={85} />
-        <Skills skill="HTML/CSS" progress={70} />
-        <Skills skill="JavaScript" progress={50} />
-        <Skills skill="NodeJS" progress={40} />
-        <Skills skill="React" progress={25} />
-        <hr style={{ borderTop: '3px solid #780206' }} />
+  <Grid>
+    <Cell col={4}>
+      <Portrait>
+        <Portrait.Image />
+        <H2>Willie Liwa Johnson</H2>
+        <Portrait.Title>Software Developer</Portrait.Title>
+        <Portrait.Split />
+        <P>
+          I'm a software engineer who specializes in iOS Development. With a
+          variety of projects under my belt I have experience using many
+          different technologies. I love to develop video games, productivity
+          apps, augmented reality apps, and content driven websites.
+        </P>
+        <Portrait.Split />
+        <H5>Address</H5>
+        <P>
+          <a
+            href="https://www.google.com/maps/place/MONTGOMRY+VLG,+MD+20879/@39.1708499,-77.1858614,14.74z/data=!4m5!3m4!1s0x89b62b579a467b6d:0xe2ea76d8df38b7a8!8m2!3d39.1733025!4d-77.1772737"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Gaithersburg, MD 20879
+          </a>
+        </P>
+        <H5>Phone</H5>
+        <P>
+          <a href="tel:240-550-5589" rel="noopener noreferrer" target="_blank">
+            (240) 550-5589
+          </a>
+        </P>
+        <H5>Email</H5>
+        <P>
+          <a
+            href={"mailto:liwa.johnson@gmail.com?subject=Let's get in touch"}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            liwa.johnson@gmail.com
+          </a>
+        </P>
+        <H5>Web</H5>
+        <P>
+          <a
+            href="http://unii.design"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            unii.design
+          </a>
+        </P>
+        <Portrait.Split />
+      </Portrait>
+    </Cell>
+    <History col={8}>
+      {/* Skills */}
+      <H2>Skills</H2>
+      <Skills skill="iOS/Swift" progress={100} />
+      <Skills skill="Unity" progress={85} />
+      <Skills skill="HTML/CSS" progress={70} />
+      <Skills skill="JavaScript" progress={50} />
+      <Skills skill="NodeJS" progress={40} />
+      <Skills skill="React" progress={25} />
+      <History.Split />
 
-        {/* Education */}
-        <h2 style={{ fontFamily: 'Raleway' }}>Education</h2>
-        <Education
-          startYear={2017}
-          endYear={2019}
-          schoolName="Make School Product College"
-          schoolDescription="Developed numerous iOS apps working with Swift, Python & Flask and Ruby & Rails to acquire knowledge of Full Stack iOS development and touching on multithreading and advanced UIKit.
+      {/* Education */}
+      <h2 style={{ fontFamily: 'Raleway' }}>Education</h2>
+      <Education
+        startYear={2017}
+        endYear={2019}
+        schoolName="Make School Product College"
+        schoolDescription="Developed numerous iOS apps working with Swift, Python & Flask and Ruby & Rails to acquire knowledge of Full Stack iOS development and touching on multithreading and advanced UIKit.
 Implemented CS Data Structures using Python and analyzed their Big-O complexity. 
 Collaborated with many developers on projects with teams of 4+ members and in the community."
-        />
-        <hr style={{ borderTop: '3px solid #780206' }} />
+      />
+      <History.Split />
 
-        {/* Experience */}
-        <h2 style={{ fontFamily: 'Raleway' }}>Experience</h2>
-        <Experience
-          startYear={2018}
-          endYear={2018}
-          jobName="Make School Summer Academy Curriculum Assistant"
-          jobDescription="Wrote iOS tutorials for Make School using markdown. Created a tutorial to help explain the process of networking and decoding JSON in swift 4."
-        />
-      </Cell>
-    </Grid>
-  </div>
+      {/* Experience */}
+      <H2>Experience</H2>
+      <Experience
+        startYear={2018}
+        endYear={2018}
+        jobName="Make School Summer Academy Curriculum Assistant"
+        jobDescription="Wrote iOS tutorials for Make School using markdown. Created a tutorial to help explain the process of networking and decoding JSON in swift 4."
+      />
+    </History>
+  </Grid>
 );
+
 export default Resume;
