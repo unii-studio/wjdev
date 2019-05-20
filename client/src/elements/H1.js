0faf8442aa63cc4f045be91b4ffeb351d4be7c80
willieLjohnson <liwa.johnson@gmail.com>
2019-05-20 14:47:38 -0400

    Update H1 element

---
+++ client/src/elements/H1.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  font-size: 44;
+  font-weight: bold;
+  color: black;
+  font-family: 'Raleway', Sans-Serif;
+`;
+
+export default H1;
