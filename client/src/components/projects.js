a778697ad1ed58198f9871fecfb82cd273f4f9aa
willieLjohnson <liwa.johnson@gmail.com>
2019-05-20 15:13:40 -0400

    Refactor projects.js using Project block

--- client/src/components/projects.js
+++ client/src/components/projects.js
@@ -14,6 +14,9 @@ import {
 } from 'react-mdl';
 
 import styled from 'styled-components';
+import H1 from '../elements/H1';
+
+import Project from '../blocks/Project';
 
 const Div = styled.div`
   display: flex;
@@ -33,44 +36,29 @@ class Projects extends Component {
       return (
         <Div>
           {/* Project 1 */}
-          <Card shadow={5} style={{ margin: 'auto' }}>
-            <CardTitle
-              style={{
-                color: '#fff',
-                height: '176px',
-                background:
-                  'url(https://i.imgur.com/lFuuL5V.jpg) no-repeat center center',
-                backgroundSize: 'cover',
-              }}
-            >
+          <Project>
+            <Project.Title img="url(https://i.imgur.com/lFuuL5V.jpg) no-repeat center center">
               Vybes
-            </CardTitle>
-            <CardText>
+            </Project.Title>
+            <Project.Text>
               A journal and meditation app designed to help you find calm in
               your daily life. Designed and developed by yours truly.
-            </CardText>
-            <CardActions border>
-              <Button
-                colored
-                href="https://github.com/willieLjohnson/Vybes"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                GitHub
-              </Button>
-              <Button
-                colored
-                href="https://itunes.apple.com/us/developer/willie-johnson/id1384634583?mt=8"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                AppStore
-              </Button>
-            </CardActions>
+            </Project.Text>
+            <Project.Actions>
+              <Project.ButtonLink
+                text="GitHub"
+                link="https://github.com/willieLjohnson/Vybes"
+              />
+              <Project.Spacer />
+              <Project.ButtonLink
+                text="iOS"
+                link="https://itunes.apple.com/us/developer/willie-johnson/id1384634583?mt=8"
+              />
+            </Project.Actions>
             <CardMenu style={{ color: '#fff' }}>
               <IconButton name="share" />
             </CardMenu>
-          </Card>
+          </Project>
         </Div>
       );
     }
@@ -80,52 +68,32 @@ class Projects extends Component {
       return (
         <Div>
           {/* Project 1 */}
-          <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-            <CardTitle
-              style={{
-                color: '#fff',
-                height: '176px',
-                background:
-                  'url(https://i.imgur.com/LhwFdwI.gif) no-repeat center center',
-                backgroundSize: 'cover',
-              }}
-            >
+          <Project>
+            <Project.Title img="url(https://i.imgur.com/LhwFdwI.gif) no-repeat center center">
               Surge
-            </CardTitle>
-            <CardText>
+            </Project.Title>
+            <Project.Text>
               Super intense arcade game inspired by Pong. Music changes with
               difficult and there are a variety of levels to get high-scores in.
-            </CardText>
-            <CardActions border>
-              <Button
-                colored
-                href="https://github.com/willieLjohnson/Surge"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                GitHub
-              </Button>
-              <Button
-                href="https://itunes.apple.com/US/app/id1392635397?mt=8"
-                colored
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                AppStore
-              </Button>
-              <Button
-                href="https://play.google.com/store/apps/details?id=com.willynillygames.surge"
-                colored
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                PlayStore
-              </Button>
-            </CardActions>
+            </Project.Text>
+            <Project.Actions>
+              <Project.ButtonLink
+                text="GitHub"
+                link="https://github.com/willieLjohnson/Surge"
+              />
+              <Project.ButtonLink
+                text="iOS"
+                link="https://itunes.apple.com/US/app/id1392635397?mt=8"
+              />
+              <Project.ButtonLink
+                text="Android"
+                link="https://play.google.com/store/apps/details?id=com.willynillygames.surge"
+              />
+            </Project.Actions>
             <CardMenu style={{ color: '#fff' }}>
               <IconButton name="share" />
             </CardMenu>
-          </Card>
+          </Project>
         </Div>
       );
     }
@@ -135,7 +103,7 @@ class Projects extends Component {
       return (
         <Div>
           {/* Project 1 */}
-          <h1> This is react</h1>
+          <H1>This is react</H1>
         </Div>
       );
     }
@@ -144,14 +112,14 @@ class Projects extends Component {
     if (activeTab === 3) {
       return (
         <Div>
-          <h1>This is MongoDB</h1>
+          <H1>This is MongoDB</H1>
         </Div>
       );
     }
 
     return (
       <Div>
-        <h1>404! How'd you get all the way here!?</h1>
+        <H1>404! How'd you get all the way here!?</H1>
       </Div>
     );
   };
