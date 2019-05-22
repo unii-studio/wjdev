67ce9d6ec90977a1cb6b477c4c9d7b1ad3aa2a04
willieLjohnson <liwa.johnson@gmail.com>
2019-05-21 23:37:18 -0400

    Refactor ButtonLink.js

--- client/src/blocks/Project/ButtonLink.js
+++ client/src/blocks/Project/ButtonLink.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Button } from 'react-mdl';
+import { string } from 'prop-types';
 
-const ButtonLink = props => (
-  <Button colored href={props.link} rel="noopener noreferrer" target="_blank">
-    {props.text}
+const ButtonLink = ({ text, link }) => (
+  <Button colored href={link} rel="noopener noreferrer" target="_blank">
+    {text}
   </Button>
 );
+
+ButtonLink.propTypes = {
+  text: string.isRequired,
+  link: string,
+};
+
+ButtonLink.defaultProps = {
+  link: '#',
+};
 
 export default ButtonLink;
