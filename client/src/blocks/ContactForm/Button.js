ccf68b39e7c66ca6b9fbeabfa018b232f0767381
willieLjohnson <liwa.johnson@gmail.com>
2019-05-21 22:51:27 -0400

    Update styled form button to look cooler with animations 8)

--- client/src/blocks/ContactForm/Button.js
+++ client/src/blocks/ContactForm/Button.js
@@ -5,16 +5,19 @@ const Button = styled.button`
   height: 40px;
   justify-self: center;
   font-family: 'Raleway', Sans-Serif;
+  background-color: #061161;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  transition: width 0.2s ease;
 
-  .btn-primary,
-  .btn-primary:hover,
-  .btn-primary:active,
-  .btn-primary:visited {
-    background-color: #061161 !important;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    font-size: 16px;
+  &:hover {
+    width: 62%;
+  }
+
+  &:active {
+    width: 65%;
   }
 `;
 
