103f5377b8814660e1d9a1161ddf8dbe7b91b31a
willieLjohnson <liwa.johnson@gmail.com>
2019-05-21 23:43:31 -0400

    Refactor Project/Text

--- client/src/blocks/Project/Text.js
+++ client/src/blocks/Project/Text.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { CardText } from 'react-mdl';
+import { oneOfType, arrayOf, node } from 'prop-types';
 
 const TextStyle = {
   fontFamily: 'Open Sans',
   fontSize: '15px',
 };
 
-const Text = props => <CardText style={TextStyle}>{props.children}</CardText>;
+const Text = ({ children }) => (
+  <CardText style={TextStyle}>{children}</CardText>
+);
+
+Text.propTypes = {
+  children: oneOfType([arrayOf(node), node]).isRequired,
+};
 
 export default Text;
